8dd2373dbc931ca5b789fd7adddfa94afb5c0b3e
YoramHernalsteen <r0582769@student.thomasmore.be>
2020-10-28 15:05:22 +0100

    improved styling

--- src/components/note.js
+++ src/components/note.js
@@ -1,14 +1,25 @@
 import React from "react";
+import styled from "@emotion/styled";
 import {MdDelete} from "react-icons/md";
 import {MdEdit} from "react-icons/md";
 import Card from "react-bootstrap/Card";
 
+const SpanLeft = styled.span`
+  float: left;
+`;
+const SpanRight = styled.span`
+ float: right;
+`;
+const Styledp = styled.p`
+width: 100%;
+`;
 export function NoteI(props){
     const{note}= props;
     return <>
         <Card>
             <Card.Header>
-                {note.category}
+                <SpanLeft>{note.category}</SpanLeft>
+                <SpanRight>{note.status}</SpanRight>
             </Card.Header>
             <Card.Body>
                 <Card.Title>
@@ -19,7 +30,7 @@ export function NoteI(props){
                 </Card.Text>
             </Card.Body>
             <Card.Footer>
-                {note.status}
+                <Styledp><SpanLeft><MdEdit/></SpanLeft><SpanRight><MdDelete/></SpanRight></Styledp>
             </Card.Footer>
         </Card>
     </>
